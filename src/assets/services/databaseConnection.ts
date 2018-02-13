f95c7f43da6d7a5052e2feaa8ec77bb14622bffd
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2018-02-13 17:57:42 +0100

    app working with all specs

---
+++ src/assets/services/databaseConnection.ts
@@ -0,0 +1,63 @@
+import {Injectable} from "@angular/core";
+declare var trafficMeister: any;
+
+@Injectable()
+export class databaseConnection {
+  static database: any;
+  
+
+  constructor() {
+  }
+
+  static initDatabase(callback) {
+    trafficMeister.fetchData((err, data) => {
+      const fetchData = data;
+      this.database = !err ? fetchData : [];
+      callback(fetchData);
+    });
+  }
+  
+  static getOptions(): Array<string> {
+    let options:Array<string> = [];
+    let execeptions:Array<String> = ['id','img'];
+    const data = this.database[0];
+    Object.keys(data).forEach(option=>{
+      if(!execeptions.includes(option)){
+        options.push(option);
+      }
+    });
+    return options;
+  }
+
+  static getOptionsValues(values): any {
+    let filtered = this.database.filter(elem => {
+      let includes = true;
+      Object.keys(values).forEach(val=>{
+        includes = includes && (values[val] ? elem[val].includes(values[val]) : true);
+      });
+      return includes;
+    });
+    let result = {};
+    if(filtered.length===1){
+      result["img"] = filtered[0].img;
+    }
+    filtered.forEach(elem=>{
+      Object.keys(values).forEach(value => {
+        result[value] = result[value]?result[value]:[];
+        if(typeof(elem[value])==="string"){
+          if(!result[value].includes(elem[value]))
+            result[value].push(elem[value]);
+        }else{
+          elem[value].forEach(listElement => {            
+            if(!result[value].includes(listElement))
+            result[value].push(listElement);
+          });
+        }
+      });
+    });
+    return result;
+  }
+  static getDatabase(): any {
+    return this.database;
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
+import { databaseConnection } from '../../assets/services/databaseConnection';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  finalVehiclePicture: string;
+  optionsLabels: string[];
+  optionsValues:Object;
+  selectValues: Object;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public loadingCtrl: LoadingController,
+  ) {
+    this.restart();
   }
 
+  initOptions(){
+    this.optionsLabels = databaseConnection.getOptions();
+    let values = {};
+    this.optionsLabels.forEach(option=>{
+      values[option] = "";
+    });
+    this.optionsValues=values;
+    this.selectValues = databaseConnection.getOptionsValues(values);
+  }
+
+  handleOption(){
+    this.selectValues = databaseConnection.getOptionsValues(this.optionsValues);
+    if(this.selectValues['img']){
+      let finalVehiclePicture = this.selectValues['img'];
+      this.finalVehiclePicture = finalVehiclePicture;
+    }
+  }
+
+  restart(){
+    this.selectValues = {};
+    this.optionsLabels = [];
+    this.optionsValues = {};
+    this.finalVehiclePicture = "";
+    let loader = this.loadingCtrl.create({
+      content:'Loading...'
+    });
+    loader.present();
+    databaseConnection.initDatabase(data => {
+      loader.dismiss();
+      this.initOptions();
+    });
+  }
 }
